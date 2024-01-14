1fa8fd1a9c839fef5f9b829cfa9b1dcabb7481f1
Maksim <156239720+MaksimG193@users.noreply.github.com>
2024-01-14 15:52:22 +0300

    Add files via upload
    Составление алгоритма: если введенное число больше 7, то вывести “Привет”

---
+++ taskn1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+ 
+public class TaskN1{
+  public static void main(String[] args) { 
+      Scanner scan = new Scanner(System.in); 
+      System.out.println("Enter number "); 
+      int number = scan.nextInt(); 
+      if (number > 7) { 
+          System.out.println("Hello"); 
+      } 
+  } 
+}
