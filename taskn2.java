4512b82216c32aa4fcd2ffe83471e8cd5e4374bf
Maksim <156239720+MaksimG193@users.noreply.github.com>
2024-01-14 15:53:21 +0300

    Add files via upload
    Составление алгоритма: если введенное имя совпадает с Вячеслав, то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"

---
+++ taskn2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+ 
+public class TaskN2 { 
+  public static void main(String[] args) { 
+      Scanner scan = new Scanner(System.in); 
+      System.out.println("Введите имя: "); 
+      String name = scan.nextLine(); 
+      if (name.equals("Вячеслав")) { 
+          System.out.println("Привет, Вячеслав"); 
+      } else { 
+          System.out.println("Нет такого имени"); 
+      } 
+  } 
+}
